bc0aaf7a502d3e8da515570a591079f75edd456e
Peter Doherty <pdoherty926@gmail.com>
2016-04-15 17:11:22 -0400

    first pass at using redis sorted set for leaderboard

--- rails-5-action-cable-docker/app/channels/scores_channel.rb
+++ rails-5-action-cable-docker/app/channels/scores_channel.rb
@@ -3,12 +3,19 @@ class ScoresChannel < ApplicationCable::Channel
     stream_from "scores_channel"
   end
 
-  def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
-  end
+  def unsubscribed; end
 
   def simulate(message)
-    ActionCable.server.broadcast 'scores_channel', message: message
-    # TestJob.perform_now data
+
+    redis = Redis.new
+    redis.zadd 'scores',
+               message['score'],
+               message['initials']
+
+    scores = redis.zrevrange 'scores', 0, 10, with_scores: true
+
+    ActionCable.server.broadcast 'scores_channel',
+                                 message: scores
+
   end
 end
